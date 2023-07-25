2bb3a7e8f79ae1c301c26f97afef11b107c21b1f
Blaque <blaque.develop@gmail.com>
2023-07-25 21:12:22 +0300

    Done with assignment

---
+++ src/components/SocialMedia.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function SocialMedia({ socialLinks }) {
+    return (
+      <div>
+        <a href={socialLinks.github}>{socialLinks.github}</a>
+        <a href={socialLinks.linkedin}>{socialLinks.linkedin}</a>
+      </div>
+    );
+  }
+
+  export default SocialMedia
